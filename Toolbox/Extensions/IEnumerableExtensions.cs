38589ac478912693988276223caf10c9ba793f86
Stig Voss <stig.voss@gmail.com>
2016-11-21 12:13:59 +0200

    Added several extension methods

---
+++ Toolbox/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolbox.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> collection)
+        {
+            return new HashSet<T>(collection);
+        }
+
+        public static HashSet<TResult> ToHashSet<TSource, TResult>(this IEnumerable<TSource> collection, Func<TSource, TResult> selector = null)
+        {
+            return new HashSet<TResult>(collection.Select(selector));
+        }
+    }
+}
---
+++ Toolbox/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolbox.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToTitleCase(this string text)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text.ToLower());
+        }
+    }
+}
