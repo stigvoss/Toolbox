c747b7017d754661f6eadc85cdba85a75ce280ae
Stig Voss <stig.voss@gmail.com>
2016-12-18 21:52:21 +0200

    Added Toolbox.Oracle for Oracle.ManagedDataAccess.Client Added extensions for OracleCommand Added extensions for DbCommand Added extensions for DbConnection

---
+++ Toolbox/Extensions/DbConnectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolbox.Extensions
+{
+    public static class DbConnectionExtensions
+    {
+        public static DbCommand CreateCommand(this DbConnection connection, string commandText)
+        {
+            var command = connection.CreateCommand();
+
+            command.CommandText = commandText;
+
+            return command;
+        }
+    }
+}
