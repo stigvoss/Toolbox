8aa60d2bfa4feffcce0095dbd8b4340e84e19137
Stig Voss <stig@stigvoss.dk>
2018-06-27 15:21:43 +0200

    Adds IDataflowBlockExtensions

---
+++ Source/Toolbox/Toolbox.Threading.Tasks.Dataflow/Extensions/IDataflowBlockExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks.Dataflow;
+
+namespace Toolbox.Threading.Tasks.Dataflow.Extensions
+{
+    public static class IDataflowBlockExtensions
+    {
+        public static void LinkTo<T>(this IDataflowBlock source, ITargetBlock<T> target, DataflowLinkOptions options)
+        {
+            source.Completion.ContinueWith(task =>
+            {
+                if (options.PropagateCompletion)
+                {
+                    target.Complete();
+                }
+            });
+        }
+    }
+}
