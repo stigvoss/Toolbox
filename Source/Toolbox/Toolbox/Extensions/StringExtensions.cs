51e5f493f351a7b12dddb964ab7be939192e8081
Emil Jensen <estj92@gmail.com>
2019-04-21 19:15:29 +0200

    Handle nulls in StringExtensions after upgrade to C# 8.0
    GH-7

--- Source/Toolbox/Toolbox/Extensions/StringExtensions.cs
+++ Source/Toolbox/Toolbox/Extensions/StringExtensions.cs
@@ -11,21 +11,11 @@ namespace Toolbox.Extensions
     {
         public static string ToTitleCase(this string text)
         {
-            if (text is null)
-            {
-                return null;
-            }
-
             return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text.ToLower());
         }
 
-        public static byte[] ToBytes(this string value, Encoding encoding = null)
+        public static byte[] ToBytes(this string value, Encoding? encoding = null)
         {
-            if (value is null)
-            {
-                return null;
-            }
-
             if (encoding is null)
             {
                 encoding = Encoding.UTF8;
