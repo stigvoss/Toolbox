e72b14de505fc498db1e0d4d1699c28290becedc
Emil Jensen <estj92@gmail.com>
2019-04-24 15:26:13 +0200

    Rename tests based on comments

--- Source/Toolbox/Toolbox.Extensions.Tests/An_IEnumerable.cs
+++ Source/Toolbox/Toolbox.Extensions.Tests/An_IEnumerable.cs
@@ -10,7 +10,7 @@ namespace Toolbox.Extensions.Tests
     public class An_IEnumerable
     {
         [Test]
-        public void Can_Convert_To_ToConcurrentHashSet()
+        public void Can_Convert_Using_ToConcurrentHashSet()
         {
             var data = Enumerable.Range(0, 4).ToConcurrentHashSet();
             var expected = new int[] { 0, 1, 2, 3 };
@@ -18,7 +18,7 @@ namespace Toolbox.Extensions.Tests
         }
 
         [Test]
-        public void Can_Convert_To_ToConcurrentHashSet_With_Selector()
+        public void Can_Convert_Using_ToConcurrentHashSet_With_Selector()
         {
             var data = Enumerable.Range(0, 4).ToConcurrentHashSet(x => x * 2);
             var expected = new int[] { 0, 2, 4, 6 };
